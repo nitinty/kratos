1bdff4bf83ae8d5d7f236647154b523c31eac645
chaitanya.singla <chaitanya.singla@telekom-digital.com>
2025-09-18 12:55:13 +0530

    Add reconnect logic when websocket connection breaks

--- client.go
+++ client.go
@@ -2,7 +2,9 @@ package kratos
 
 import (
 	"sync"
+	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/xmidt-org/wrp-go/v3"
 	"go.uber.org/zap"
 )
@@ -34,6 +36,9 @@ type client struct {
 	wg              sync.WaitGroup
 	pingConfig      PingConfig
 	once            sync.Once
+	config          ClientConfig
+	pinged          chan string
+	connMu          sync.Mutex
 }
 
 // used to track everything that we want to know about the client headers
@@ -42,7 +47,7 @@ type clientHeader struct {
 	firmwareName string
 	modelName    string
 	manufacturer string
-	token string
+	token        string
 }
 
 // websocketConnection maintains the websocket connection upstream (to XMiDT).
@@ -87,6 +92,31 @@ func (c *client) Close() error {
 	return connectionErr
 }
 
+// // going to be used to access the HandleMessage() function
+// func (c *client) read() {
+// 	defer c.wg.Done()
+// 	c.logger.Info("Watching socket for messages.")
+
+// 	for {
+// 		select {
+// 		case <-c.done:
+// 			c.logger.Info("Stopped reading from socket.")
+// 			return
+// 		default:
+// 			c.logger.Info("Reading message...")
+
+// 			_, serverMessage, err := c.connection.ReadMessage()
+// 			if err != nil {
+// 				c.logger.Error("Failed to read message. Exiting out of read loop.", zap.Error(err))
+// 				return
+// 			}
+// 			c.decoderSender.DecodeAndSend(serverMessage)
+
+// 			c.logger.Debug("Message sent to be decoded")
+// 		}
+// 	}
+// }
+
 // going to be used to access the HandleMessage() function
 func (c *client) read() {
 	defer c.wg.Done()
@@ -98,16 +128,68 @@ func (c *client) read() {
 			c.logger.Info("Stopped reading from socket.")
 			return
 		default:
-			c.logger.Info("Reading message...")
-
-			_, serverMessage, err := c.connection.ReadMessage()
-			if err != nil {
-				c.logger.Error("Failed to read message. Exiting out of read loop.", zap.Error(err))
-				return
-			}
-			c.decoderSender.DecodeAndSend(serverMessage)
-
-			c.logger.Debug("Message sent to be decoded")
+			c.logger.Debug("Reading message...")
 		}
+
+		c.connMu.Lock()
+		conn := c.connection
+		c.connMu.Unlock()
+		if conn == nil {
+			c.logger.Warn("connection is nil, attempting reconnect")
+			c.attemptReconnect()
+			continue
+		}
+
+		_, serverMessage, err := conn.ReadMessage()
+		if err != nil {
+			c.logger.Error("Failed to read message. Will attempt to reconnect.", zap.Error(err))
+			c.attemptReconnect()
+			continue
+		}
+
+		c.decoderSender.DecodeAndSend(serverMessage)
+		c.logger.Debug("Message sent to be decoded")
+	}
+}
+
+func (c *client) attemptReconnect() {
+	backoff := time.Second
+
+	for {
+		select {
+		case <-c.done:
+			c.logger.Info("Stop requested; aborting reconnect attempts")
+			return
+		default:
+		}
+
+		c.logger.Info("Trying to reconnect websocket...")
+
+		// use stored client config and header info
+		conn, wsURL, err := createConnection(c.headerInfo, c.config)
+		if err != nil {
+			c.logger.Info("reconnect attempt failed", zap.Error(err), zap.Duration("retryIn", backoff))
+			time.Sleep(backoff)
+			if backoff < 30*time.Second {
+				backoff *= 2
+			}
+			continue
+		}
+		// reinstall ping handler to send into the same ping channel
+		conn.SetPingHandler(func(appData string) error {
+			c.pinged <- appData
+			return conn.WriteControl(websocket.PongMessage, []byte(appData), time.Now().Add(writeWait))
+		})
+
+		c.connMu.Lock()
+		old := c.connection
+		c.connection = conn
+		c.connMu.Unlock()
+		if old != nil {
+			_ = old.Close()
+		}
+
+		c.logger.Info("websocket reconnected", zap.String("url", wsURL))
+		return
 	}
 }
