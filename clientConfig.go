f9c6d9d607783e90092a2b87793b235c02ab114a
chaitanya.singla <chaitanya.singla@telekom-digital.com>
2025-09-18 14:56:23 +0530

    Remove extra log

--- clientConfig.go
+++ clientConfig.go
@@ -157,7 +157,6 @@ func createConnection(headerInfo *clientHeader, config ClientConfig) (connection
 		return nil, "", err
 	}
 	var talariaInstance = ""
-	fmt.Println("Creating connection for Device Name: ", config.DeviceName)
 	tlsConfig := GetTLSConfig(strings.Split(config.DeviceName, ":")[1], config.CertificatesPath, config.UseSSL)
 	if config.PetasosEnabled {
 		talariaInstance, err = getTalariaInstance(config, tlsConfig)
